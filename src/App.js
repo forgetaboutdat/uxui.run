7e5dc872394775da0444bc94bdbb3ad09575a255
forgetaboutdat <111140666+forgetaboutdat@users.noreply.github.com>
2022-12-19 14:49:45 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/fVwvpCs-8dme7er7/scene.splinecode" />
+  );
+}
